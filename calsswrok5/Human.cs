55aa3a4153b816800590cf32cf853c42cf096851
Егор Марков <rupor.222@yandex.ru>
2022-04-19 19:39:36 +0300

    Добавьте файлы проекта.

---
+++ calsswrok5/Human.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calsswrok5
+{
+    internal class Human
+    {
+        /* public Human()
+         {
+             Name = "Иван";
+             SecondName = "Иванов";
+             FullName = "Иван Иванов";
+             year = DateTime.Now.Year;
+             month = DateTime.Now.Month; 
+             day = DateTime.Now.Day;
+
+         }
+        */
+        private string _name;
+        private string _secondName;
+        private int _year;
+        private int _month;
+        private int _day;
+        public Human(string name1, string secondname1,  int year1, int month1, int day1)
+        {
+            Name = name1;
+            SecondName = secondname1;
+            year = year1;
+            month = month1;
+            day = day1;
+            
+        }
+        public string Name 
+        { 
+            get
+            {
+                return _name;
+            } 
+            set
+            {
+                
+                if (value == null)
+                {
+                    _name = "Иван";
+                } else
+                {
+                    _name = value;
+                }
+            }
+            
+        }
+        
+   
+        public string SecondName { 
+            get
+            {
+                return _secondName;
+            }
+            set
+            { 
+                if (value == null)
+                {
+                    _secondName = "Иванов";
+                } else
+                {
+                    _secondName = value;
+                }
+            }
+        }
+       
+        
+        public int year
+        {
+            get
+            {
+                return _year;
+            }
+
+
+            set
+            { 
+                if (value < 1900)
+                {
+                    _year = 1900;
+                }
+                else if (value > DateTime.Now.Year)
+                {
+                    _year = DateTime.Now.Year;
+                }
+                else
+                {
+                    _year = value;
+                }
+            }
+        }
+       
+        public int month {
+            get
+            {
+                return _month;
+            }
+
+
+            set
+            {
+                if (value < 0)
+                {
+                    _month = 1;
+                }
+                else if (value > 12)
+                {
+                    _month = 12;
+                }
+                else
+                {
+                    _month = value;
+                }
+            }
+        }
+        
+        public int day {
+            get
+            {
+                return _day;
+            }
+
+
+            set
+            {
+                if (value < 0)
+                {
+                    _day = 1;
+                }
+                else if (value > 31)
+                {
+                    _day = 31;
+                }
+                else
+                {
+                    _day = value;
+                }
+            }
+        }
+
+        public string FullName 
+        {
+             get
+            {                
+                
+                return Name + " " + SecondName;
+            }
+         }
+        public int Age
+        {
+            get
+            {
+                DateTime now = DateTime.Today;
+                return now.Year - year -  (now.Month> month || now.Month == month && now.Day>= day ? 0:1);
+            }
+    }
+        
+     public void AllInfo()
+        {
+            Console.Clear();
+            Console.WriteLine(FullName);
+            Console.WriteLine($"Вам {Age} лет");
+
+        }   
+     
+    }
+}
---
+++ calsswrok5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace calsswrok5// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите имя");
+            
+            string Name =Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            string SecondName =Console.ReadLine();
+            Console.WriteLine("Введите день вашего рождения");
+            int.TryParse(Console.ReadLine(), out int i);
+            
+            Console.WriteLine("Введите месяц вашего рождения");
+            int.TryParse(Console.ReadLine(), out int y);
+            
+            Console.WriteLine("Введите год вашего рождения");
+            int.TryParse(Console.ReadLine(), out int z);
+           
+            Human human = new Human(Name, SecondName, i, y, z);
+            human.AllInfo();
+        }
+    }
+}
